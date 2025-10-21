62a566a01e15f066a2c5fd85ccb4f3519b3227a9
kimgwonhee <lovezzao9876@gmail.com>
2025-10-22 03:25:03 +0900

    삽입,찾기,최대최소 구현

--- rbtree_lab/src/rbtree.c
+++ rbtree_lab/src/rbtree.c
@@ -4,6 +4,7 @@
 
 rbtree *new_rbtree(void)
 {
+  // TODO: initialize struct if needed
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
 
   // sentinel(nil) 노드 생성 및 초기화
@@ -13,7 +14,6 @@ rbtree *new_rbtree(void)
   p->nil = nil_node;
   p->root = p->nil; // root 초기값도 sentinel로 설정
   return p;
-  // TODO: initialize struct if needed
 }
 
 void delete_rbtree(rbtree *t)
@@ -67,20 +67,60 @@ node_t *rbtree_insert(rbtree *t, const key_t key)
 
 node_t *rbtree_find(const rbtree *t, const key_t key)
 {
-  // TODO: implement find
-  return t->root;
+  // 트리의 루트 노드부터 탐색 시작
+  node_t *cur = t->root;
+
+  // 현재 노드가 nil(트리의 끝)이 아닐 때까지 반복
+  while (cur != t->nil)
+  {
+    // 현재 노드의 키 값이 찾는 값과 같으면 해당 노드를 반환
+    if (key == cur->key)
+    {
+      return cur;
+    }
+    // 찾는 값이 현재 노드보다 작으면 왼쪽 자식으로 이동
+    else if (key < cur->key)
+    {
+      cur = cur->left;
+    }
+    // 찾는 값이 현재 노드보다 크면 오른쪽 자식으로 이동
+    else
+    {
+      cur = cur->right;
+    }
+  }
+  // 값을 찾지 못한 경우 nil 노드 반환
+  return t->nil;
 }
 
 node_t *rbtree_min(const rbtree *t)
 {
   // TODO: implement find
-  return t->root;
+  node_t *cur = t->root;
+
+  if (cur == t->nil) // 빈 트리면 nil 반환
+    return t->nil;
+
+  while (cur->left != t->nil)
+  {
+    cur = cur->left;
+  }
+  return cur;
 }
 
 node_t *rbtree_max(const rbtree *t)
 {
   // TODO: implement find
-  return t->root;
+  node_t *cur = t->root;
+
+  if (cur == t->nil)
+    return t->nil;
+
+  while (cur->right != t->nil)
+  {
+    cur = cur->right;
+  }
+  return cur;
 }
 
 int rbtree_erase(rbtree *t, node_t *p)
